fed927875eb924fbff97862c8d71e8b957ba2fa6
Tim Gifford <timgifford@granular.ag>
2018-08-06 10:09:12 -0700

    delete the docker image before starting it

--- do/local-docker.sh
+++ do/local-docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Delete the db2 image
+docker-compose rm -f db2
 docker-compose up --build --force-recreate
 
 #docker run -it -p 50000:50000 -e DB2INST1_PASSWORD=db2inst1-pwd -e LICENSE=accept ibmcom/db2express-c:latest bash
